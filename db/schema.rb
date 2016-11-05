d27e60ee94523d20d33e67d4198b30fc7330a2ac
Sam Leiken <sam.k.leiken@gmail.com>
2016-11-04 20:51:27 -0700

    Change hours column to pending_hours and approved_hours columns. Add seeds directory that loads all files on 'rails db:seed'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161104224627) do
+ActiveRecord::Schema.define(version: 20161105034820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -116,11 +116,12 @@ ActiveRecord::Schema.define(version: 20161104224627) do
   end
 
   create_table "registrations", force: :cascade do |t|
-    t.integer  "hours",      default: 0
     t.integer  "event_id"
     t.integer  "giver_id"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.integer  "pending_hours"
+    t.integer  "approved_hours"
   end
 
   create_table "subscriptions", force: :cascade do |t|
