773e8d55d7d2d7dab75763b7db89f8b0ec0e01d1
Sam Leiken <sam.k.leiken@gmail.com>
2016-11-05 15:37:59 -0700

    Remove comments from routes config

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   devise_for :managers
   devise_for :givers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
