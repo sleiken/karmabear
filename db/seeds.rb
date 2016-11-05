293928a33ca19824bc3a4511bbacad464e3f75bb
Sam Leiken <sam.k.leiken@gmail.com>
2016-11-05 00:09:39 -0700

    Update seeds.rb to use regex to skip files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
 SEED_FILES = Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].sort
-SKIP_FILES = Dir[File.join(Rails.root, 'db', 'seeds', 'test.rb'),
-                 File.join(Rails.root, 'db', 'seeds', 'destroy_records.rb')]
+SKIP_FILES = [/.*test.rb/, /.*destroy_records.rb/]
 
 SEED_FILES.each do |seed|
-  if !SKIP_FILES.include?(seed)
+  if !SKIP_FILES.any? { |re| re =~ seed }
     load seed
   end
 end
