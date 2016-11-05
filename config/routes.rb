50d1347c733b5426a45cddfaaa5bb68d29a0b79e
Sam Leiken <sam.k.leiken@gmail.com>
2016-11-05 16:09:34 -0700

    Add basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
   devise_for :managers
   devise_for :givers
+
+  namespace :manager do
+    resources :charities, only: [:show] do
+      resources :events
+      resources :needs
+    end
+  end
+
+  resources :charities, only: [:index, :show] do
+    resources :events, only: [:show]
+    resources :needs, only: [:show]
+  end
+
+  resources :profile, only: [:show], controller: 'givers'
 end
