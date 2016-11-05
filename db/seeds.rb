b0c648d1b07a4a3c011c9523217bd844e6edf309
Sam Leiken <sam.k.leiken@gmail.com>
2016-11-04 17:28:02 -0700

    Fix column names for event migration and add test seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,56 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Tag.create(name: "Food Bank")
+
+Charity.create(name: "SF Marin Food Bank",
+              lat:  "37.7983818054199",
+              lng:  "-122.407821655273",
+              address: ["900 Pennsylvania Ave", "", "San Francisco, CA", "94107"],
+              phone: "+14154212337",
+              url: "https://www.sfmfoodbank.org",
+              description: "A place to give back!"
+              manager_id: 1)
+
+Charity_tag.create(tag_id: 1, charity_id: 1)
+
+Manager.create(first_name: "Billy",
+              email: "manager@charity.com"
+              last_name: "Bob",
+              username: "BillyBob",
+              password: "123456",
+              charity_id: 1)
+
+Giver.create(first_name: "James",
+            last_name: "Dean",
+            username: "JDean",
+            email: "JDean@123.com",
+            password: "password"
+            points: 0)
+
+Need.create(name: "Blankets",
+           description: "Heavy blankets for the fall season!",
+           quanity_needed: 10,
+           status: "Active",
+           charity_id: 1)
+
+
+Event.create(name: "Can Food Drive",
+            start: "2016-12-25 04:10:12",
+            end: "2016-12-25 06:10:12",
+            description: "Come in and bring your cans!"
+            givers_needed: 10
+            charity_id: 1)
+
+Registration.create(event_id: 1,
+                   giver_id: 1,
+                   hours: 2)
+
+Subscription.create(charity_id: 1,
+                   giver_id: 1)
+
+Donation.create(need_id: 1,
+               giver_id: 1,
+               quanity_given: 5)
+
+Location.create(name: "Work",
+               lat: "37.7983818054199",
+               lng: "-122.407821655273",
+               giver_id: 1)
