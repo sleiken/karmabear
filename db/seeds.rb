8c432409e5970d6879185ee08a8daa60e8e36700
Sam Leiken <sam.k.leiken@gmail.com>
2016-11-04 23:16:32 -0700

    Add destroy_records to skip_files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 SEED_FILES = Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].sort
-SKIP_FILES = Dir[File.join(Rails.root, 'db', 'seeds', 'test.rb')]
+SKIP_FILES = Dir[File.join(Rails.root, 'db', 'seeds', 'test.rb'),
+                 File.join(Rails.root, 'db', 'seeds', 'destroy_records.rb')]
 
 SEED_FILES.each do |seed|
   if !SKIP_FILES.include?(seed)
