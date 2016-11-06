3c4b1a6d129c0b1db025cf97a1ebf5320ef0f113
Ali Baker <email": null>
2016-11-06 15:45:15 -0800

    Redo needs controller - model after the events controller

--- app/controllers/manager/needs_controller.rb
+++ app/controllers/manager/needs_controller.rb
@@ -1,34 +1,58 @@
 class Manager::NeedsController < Manager::ApplicationController
-  # before_action :manager
-  before_action :charity
+  before_action :find_charity
+  before_action :find_need, only: [:edit, :update, :destroy]
 
   def index
-    @needs = charity.needs
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def edit
+    @needs = @charity.needs
   end
 
   def show
-    @need = Event.find(params[:id])
+    #Display this charity from link
+    @need = @charity.needs.find(params[:id])
+  end
+
+  def new
+    @need = Need.new
+  end
+
+  def create
+    @need = @charity.needs.new(need_params)
+
+    if @need.save
+      redirect_to manager_charity_need_path(@charity, @need)
+    else
+      @errors = @need.errors.full_messages
+      render "manager/needs#show"
+    end
+  end
+
+  def edit
+
   end
 
   def update
+    @need.update(need_params)
+    redirect_to manager_charity_need_url(@charity, @need)
   end
 
   def destroy
+    @need.destroy
 
+    redirect_to manager_charity_needs_url(@charity)
   end
 
   private
 
-  def charity
-    @charity ||= Charity.find(params[:charity_id])
+  def need_params
+    params.require(:need).permit(:name, :description, :quantity_needed, :status)
   end
+
+  def find_charity
+    @charity = Charity.find(params[:charity_id])
+  end
+
+  def find_need
+    @need = Need.find(params[:id])
+  end
+
 end
