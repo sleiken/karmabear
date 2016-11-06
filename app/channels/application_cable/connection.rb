9ee9dc2ef331ed1ea3ea4a784792862a28c47d1c
Sam Leiken <sam.k.leiken@gmail.com>
2016-11-06 14:49:27 -0800

    Change giver show route BREAKS GIVER SHOW

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,14 +3,14 @@ module ApplicationCable
     identified_by :current_giver
 
     def connect
-      self.current_user = find_verified_user
+      self.current_giver = find_verified_giver
     end
 
     protected
 
-    def find_verified_user
-      if current_user = User.find_by(id: cookies.signed[:user_id])
-        current_user
+    def find_verified_giver
+      if current_giver = Giver.find_by(id: cookies.signed[:giver_id])
+        current_giver
       else
         reject_unauthorized_connection
       end
