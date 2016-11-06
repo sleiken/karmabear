ef52105557febb43eee4b7a61bfc6e6c75a79dee
Sam Leiken <sam.k.leiken@gmail.com>
2016-11-05 20:45:25 -0700

    Find current_manager in manager controllers

--- app/controllers/manager/charities_controller.rb
+++ app/controllers/manager/charities_controller.rb
@@ -2,7 +2,7 @@ class Manager::CharitiesController < Manager::ApplicationController
 	def show
 		#Show the main dashboard information, drill into associations to get view data.
 		#Should actually be current user.
-		manager = Manager.find(1)
+		manager = current_manager
 		@charity = manager.charity
 	end
 
