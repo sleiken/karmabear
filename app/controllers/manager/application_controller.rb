ae784ccc0774077eb46272081d0db8f8fc40e64c
Sam Leiken <sam.k.leiken@gmail.com>
2016-11-05 20:52:42 -0700

    Add sign in sign out buttons to nav and commetn out authorize manager method

--- app/controllers/manager/application_controller.rb
+++ app/controllers/manager/application_controller.rb
@@ -1,11 +1,11 @@
 class Manager::ApplicationController < ApplicationController
-  before_action :authorize_manager
-
-  private
-
-  def authorize_manager
-    if current_manager != Manager.find_by!(username: params[:username])
-      redirect_to root_path
-    end
-  end
+  # before_action :authorize_manager
+  #
+  # private
+  #
+  # def authorize_manager
+  #   if current_manager != Manager.find_by!(username: params[:username])
+  #     redirect_to root_path
+  #   end
+  # end
 end
