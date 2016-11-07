09eb88283ca24e799d38dce6d65e5421c1432132
Kimberly Pena <Kimberly.Pena32@gmail.com>
2016-11-06 16:06:23 -0800

    add charity seed file w/ descriptiona and image url

---
+++ db/seeds/chairtydisplay.rb
@@ -0,0 +1,204 @@
+Charity.create(name: "One Brick",
+               lat: 37.7903137207031,
+               lng: -122.404182434082,
+               address: [
+                "237 Kearny St.",
+                "Ste 209",
+                "Union Square",
+                "San Francisco, CA 94108"
+                ],
+                description: "One Brick provides support to local non-profit and community organizations by creating a unique, social and flexible volunteer environment for those interested in making a concrete difference in the community. We enable people to get involved, have an impact and have fun, without the requirements of individual long-term commitments.",
+                phone: "+1-888-412-7425",                 
+                url: "http://www.onebrick.org/",
+                image_url: "https://s3-media2.fl.yelpcdn.com/bphoto/gA_Z6iEBU2wJKZFXS7QW1Q/o.jpg"
+               )
+
+Charity.create(name: "Kiva",
+               lat: 37.7818553,
+               lng: -122.4042334,
+               address: [
+                "875 Howard St",
+                "Ste 340",
+                "SoMa",
+                "San Francisco, CA 94103"
+                ],
+                description: "Kiva is an international nonprofit, founded in 2005 and based in San Francisco, with a mission to connect people through lending to alleviate poverty. We celebrate and support people looking to create a better future for themselves, their families and their communities.",
+                phone: "+1-828-479-5482",                 
+                url: "https://www.kiva.org/",
+                image_url: "https://www-kiva-org.global.ssl.fastly.net/cms/styles/fellows_blog_excerpt/s3/sites/default/files/kivablog/philippines_update_blog.jpg?itok=aNOGL0m5"
+               )
+
+Charity.create(name: "CounterPulse",
+               lat: 37.7833779,
+               lng: -122.4104035,
+               address: [
+                "80 Turk St",
+                "Tenderloin",
+                "San Francisco, CA 94102"
+                ],
+                description: "CounterPulse is building a movement of risk-taking art that shatters assumptions and builds community. We provide space and resources for emerging artists and cultural innovators, serving as an incubator for the creation of socially relevant, community-based art and culture. CounterPulse acts as a catalyst for art and action; creating a forum for the open exchange of art and ideas, sparking transformation in our communities and our society.",
+                phone: "+1-415-626-2060",                 
+                url: "http://www.counterpulse.org/",
+                image_url: "http://www.counterpulse.org/wp-content/uploads/2016/03/CP-Red.png"
+               )
+
+Charity.create(name: "Glide",
+               lat: 37.78509311378,
+               lng: -122.41156026721,
+               address: [
+                "330 Ellis St",
+                "Tenderloin",
+                "San Francisco, CA 94102"
+                ],
+                description: "GLIDE's mission is to create a radically inclusive, just and loving community mobilized to alleviate suffering and break the cycles of poverty and marginalization.",
+                phone: "+1-415-674-6000",                 
+                url: "http://www.glide.org/",
+                image_url: "https://haasundergrad.files.wordpress.com/2013/11/locations_glide.jpg"
+               )
+Charity.create(name: "UniversalGiving",
+               lat: 37.782154,
+               lng: -122.406601,
+               address: [
+                "901 Mission St",
+                "Ste 205",
+                "SoMa",
+                "San Francisco, CA 94103"
+                ],
+                description: "UniversalGiving helps people give and volunteer with the top-performing projects all over the world.  All projects are vetted through UniversalGiving's trademarked, proprietary Quality Model. 100%\ of each donation goes directly to the cause.",
+                phone: "+1-415-296-9193",                 
+                url: "http://www.universalgiving.org/",
+                image_url: "https://pbs.twimg.com/profile_images/635948942493007872/DyEKuO1t.jpg"
+               )
+
+Charity.create(name: "Delancey Street Foundation",
+               lat: 37.7834854125977,
+               lng: -122.388893127441,
+               address: [
+                "600 The Embarcadero",
+                "Embarcadero",
+                "San Francisco, CA 94107"
+                ],
+                description: "Delancey Street is the country's leading residential self-help organization for former substance abusers, ex-convicts, homeless and others who have hit bottom.During their time at Delancey Street, residents receive a high school equivalency degree (GED) and are trained in 3 different marketable skills. Beyond academic and vocational training, residents learn important values, and the social and interpersonal skills that allow them to live successfully in the mainstream of society.",
+                phone: "+1-415-512-2950",                 
+                url: "http://www.delanceystreetfoundation.org/",
+                image_url: "http://www.delanceystreetfoundation.org/images/logoleft.gif"
+               )
+
+Charity.create(name: "At The Crossroads",
+               lat: 37.784003,
+               lng: -122.4090928,
+               address: [
+                "38 Mason St",
+                "Union Square",
+                "San Francisco, CA 94102"
+                ],
+                description: "The mission of At The Crossroads is to reach out to homeless youth and young adults at their point of need, and work with them to build healthy and fulfilling lives. Our innovative model focuses on young people who do not access traditional services and are disconnected from any type of consistent support.",
+                phone: "+1-415-487-0691",                 
+                url: "https://www.atthecrossroads.org/",
+                image_url: "http://www.ucarecdn.com/fbe6616e-86db-44d1-b117-a78e04e9b05c/-/preview/300x300/"
+               )
+
+Charity.create(name: "Golden Gate Labrador Retriever Rescue",
+               lat: 37.7910912036896,
+               lng: -122.401823848486,
+               address: [
+                "268 Bush St",
+                "Ste 4322",
+                "Financial District",
+                "San Francisco, CA 94104"
+                ],
+                description: "Our Lab Rescue team saves purebred Labrador Retrievers and Lab mixes from shelters, as well as assists people giving up their dogs for adoption.  We provide foster homes, veterinary care, and rehabilitation when needed, and place them with loving, forever families.",
+                phone: "+1-415-860-5456",                 
+                url: "http://www.labrescue.org/index.html",
+                image_url: "https://s-media-cache-ak0.pinimg.com/236x/23/aa/78/23aa78dc17a30788c7fe52c8a4d100a9.jpg"
+               )
+
+Charity.create(name: "Yerba Buena Center For the Arts",
+               lat: 37.78574,
+               lng: -122.40255,
+               address: [
+                "701 Mission St",
+                "Financial District",
+                "San Francisco, CA 94103"
+                ],
+                description: "YBCA is a new kind of art center. One that expands and extends its impact beyond its walls. A center that convenes creative souls from all walks of life to instigate actions and spur movement that transform our cities and our lives.",
+                phone: "+1-415-860-5456",                 
+                url: "https://www.ybca.org/",
+                image_url: "http://interaction15.ixda.org/wp-content/uploads/i15-partner-ybca-340x340.png"
+               )
+
+Charity.create(name: "Goethe Institut",
+               lat: 37.7907638549805,
+               lng: -122.406120300293,
+               address: [
+                "530 Bush St",
+                "2nd Floor",
+                "Chinatown",
+                "San Francisco, CA 94108"
+                ],
+                description: "We promote knowledge of the German language abroad and foster international cultural cooperation. We convey a comprehensive image of Germany by providing information about cultural, social and political life in our nation.",
+                phone: "+1-415-263-8760",                 
+                url: "https://www.goethe.de/ins/us/en/index.html",
+                image_url: "https://pbs.twimg.com/profile_images/433691134708051969/ZQ__HUpz_400x400.png"
+               )
+
+Charity.create(name: "Bayanihan Community Center",
+               lat: 37.7807,
+               lng: -122.40926,
+               address: [
+                "1010 Mission St",
+                "Ste B",
+                "SoMa",
+                "San Francisco, CA 94103"
+                ],
+                description: "The Filipino American Development Foundation exists to strengthen the social, physical, and economic well being of the Filipino American community and the South of Market community with special attention to the underserved segments of the community.",
+                phone: "+1-415-348-8042",                 
+                url: "http://bayanihancc.org/",
+                image_url: "http://ppie.ae/wp-content/uploads/2016/07/bayanihan-council.jpg"
+               )
+
+Charity.create(name: "Book Club of California",
+               lat: 37.7896823,
+               lng: -122.405848,
+               address: [
+                "312 Sutter St",
+                "Ste 500",
+                "Union Square",
+                "San Francisco, CA 94108"
+                ],
+                description: "The Book Club of California is a non-profit membership organization founded in 1912. It supports the art of fine printing related to the history and literature of California and the western states of America through research, publishing, public programs, and exhibitions."
+                phone: "+1-415-781-7532",                 
+                url: "http://www.bccbooks.org/",
+                image_url: "http://www.bccbooks.org/wp-content/themes/bookclub_child_theme/images/BCC_PressMark.png"
+               )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
