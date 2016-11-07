1042a51642eada5caffcacf17c36be850e1520ed
Ty Daniels <ty.multisonus@gmail.com>
2016-11-07 11:29:18 -0800

    Adds functions for searching and displaying from Rails DB

--- Selfie/HTTPHelper.swift
+++ Selfie/HTTPHelper.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CoreLocation
 
 enum HTTPRequestAuthType {
   case HTTPBasicAuth
@@ -19,19 +20,18 @@ enum HTTPRequestContentType {
 }
 
 struct HTTPHelper {
-  static let API_AUTH_NAME = "tydaniels"
-  static let API_AUTH_PASSWORD = "C1380C0D40A07DE25220D8931552A242D4B3544DD94C97FFCD83A7338B5763DD"
-  static let BASE_URL = "https://salty-refuge-72885.herokuapp.com/api"
+  static let BASE_URL = "http://karma-bear.herokuapp.com/api"
   
-  func buildRequest(path: String!, method: String, authType: HTTPRequestAuthType,
+  func buildRequest(path: String!, method: String,
     requestContentType: HTTPRequestContentType = HTTPRequestContentType.HTTPJsonContent, requestBoundary:String = "") -> NSMutableURLRequest {
       // 1. Create the request URL from path
       let requestURL = NSURL(string: "\(HTTPHelper.BASE_URL)/\(path)")
       let request = NSMutableURLRequest(URL: requestURL!)
+
       
       // Set HTTP request method and Content-Type
       request.HTTPMethod = method
-      
+    
       // 2. Set the correct Content-Type for the HTTP Request. This will be multipart/form-data for photo upload request and application/json for other requests in this app
       switch requestContentType {
       case .HTTPJsonContent:
@@ -40,24 +40,24 @@ struct HTTPHelper {
         let contentType = "multipart/form-data; boundary=\(requestBoundary)"
         request.addValue(contentType, forHTTPHeaderField: "Content-Type")
       }
-      
-      // 3. Set the correct Authorization header.
-      switch authType {
-      case .HTTPBasicAuth:
-        // Set BASIC authentication header
-        let basicAuthString = "\(HTTPHelper.API_AUTH_NAME):\(HTTPHelper.API_AUTH_PASSWORD)"
-        let utf8str = basicAuthString.dataUsingEncoding(NSUTF8StringEncoding)
-        let base64EncodedString = utf8str?.base64EncodedStringWithOptions(NSDataBase64EncodingOptions(rawValue: 0))
-        
-        request.addValue("Basic \(base64EncodedString!)", forHTTPHeaderField: "Authorization")
-      case .HTTPTokenAuth:
-        // Retreieve Auth_Token from Keychain
-        if let userToken = KeychainAccess.passwordForAccount("Auth_Token", service: "KeyChainService") as String? {
-          // Set Authorization header
-          request.addValue("Token token=\(userToken)", forHTTPHeaderField: "Authorization")
-        }
-      }
-      
+    
+//      // 3. Set the correct Authorization header.
+//      switch authType {
+//      case .HTTPBasicAuth:
+//        // Set BASIC authentication header
+//        let basicAuthString = "\(HTTPHelper.API_AUTH_NAME):\(HTTPHelper.API_AUTH_PASSWORD)"
+//        let utf8str = basicAuthString.dataUsingEncoding(NSUTF8StringEncoding)
+//        let base64EncodedString = utf8str?.base64EncodedStringWithOptions(NSDataBase64EncodingOptions(rawValue: 0))
+//        
+//        request.addValue("Basic \(base64EncodedString!)", forHTTPHeaderField: "Authorization")
+//      case .HTTPTokenAuth:
+//        // Retreieve Auth_Token from Keychain
+//        if let userToken = KeychainAccess.passwordForAccount("Auth_Token", service: "KeyChainService") as String? {
+//          // Set Authorization header
+//          request.addValue("Token token=\(userToken)", forHTTPHeaderField: "Authorization")
+//        }
+//    }
+    
       return request
   }
   
@@ -77,11 +77,54 @@ struct HTTPHelper {
             return
         }
         do {
+            print(data)
             completion(data, nil)
         } catch let parserError as NSError{
             print(parserError)
         }
     }
+    task.resume()
+    }
+    
+    func sendCharitySearchRequest(request: NSURLRequest, completion:([CLLocationCoordinate2D]!, NSError!) -> Void) -> () {
+        // Create a NSURLSession task
+        var charityLocations = [CLLocationCoordinate2D]()
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request) { (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+            
+            guard error == nil && data != nil else{
+                print(error)
+                completion(nil, error)
+                return
+            }
+            guard let httpResponse = response as? NSHTTPURLResponse where httpResponse.statusCode == 200 else{
+                completion(nil, error)
+                return
+            }
+            do {
+                let jsonData = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions()) as? NSArray
+                
+                    
+                    guard error == nil else {
+                        print(error)
+                        return
+                    }
+                    
+                    for coordinate in jsonData!{
+                        
+                        let json = coordinate as? [String:AnyObject]
+                        let coordinatesToAppend = CLLocationCoordinate2D(latitude: (json!["lat"]! as? Double)!, longitude: (json!["lng"]! as? Double)!)
+                        charityLocations.append(coordinatesToAppend)
+                    }
+                
+                completion(charityLocations, nil)
+            } catch let parserError as NSError{
+                print(parserError)
+            }
+        }
+        task.resume()
+    }
+
 //      if error != nil {
 //        dispatch_async(dispatch_get_main_queue(), { () -> Void in
 //          completion(data, error)
@@ -106,73 +149,79 @@ struct HTTPHelper {
 //        }
 //    }
     
-    // start the task
-    task.resume()
-  }
+    // start the tas
   
-  func uploadRequest(path: String, data: NSData, title: String) -> NSMutableURLRequest {
-    let boundary = "---------------------------14737809831466499882746641449"
-    let request = buildRequest(path, method: "POST", authType: HTTPRequestAuthType.HTTPTokenAuth,
-      requestContentType:HTTPRequestContentType.HTTPMultipartContent, requestBoundary:boundary) as NSMutableURLRequest
+//  func uploadRequest(path: String, data: NSData, title: String) -> NSMutableURLRequest {
+//    let boundary = "---------------------------14737809831466499882746641449"
+//    let request = buildRequest(path, method: "POST", authType: HTTPRequestAuthType.HTTPTokenAuth,
+//      requestContentType:HTTPRequestContentType.HTTPMultipartContent, requestBoundary:boundary) as NSMutableURLRequest
+//    
+//    let bodyParams : NSMutableData = NSMutableData()
+//    
+//    // build and format HTTP body with data
+//    // prepare for multipart form uplaod
+//    
+//    let boundaryString = "--\(boundary)\r\n"
+//    let boundaryData = boundaryString.dataUsingEncoding(NSUTF8StringEncoding) as NSData!
+//    bodyParams.appendData(boundaryData)
+//    
+//    // set the parameter name
+//    let imageMeteData = "Content-Disposition: attachment; name=\"image\"; filename=\"photo\"\r\n".dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
+//    bodyParams.appendData(imageMeteData!)
+//    
+//    // set the content type
+//    let fileContentType = "Content-Type: application/octet-stream\r\n\r\n".dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
+//    bodyParams.appendData(fileContentType!)
+//    
+//    // add the actual image data
+//    bodyParams.appendData(data)
+//    
+//    let imageDataEnding = "\r\n".dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
+//    bodyParams.appendData(imageDataEnding!)
+//    
+//    let boundaryString2 = "--\(boundary)\r\n"
+//    let boundaryData2 = boundaryString.dataUsingEncoding(NSUTF8StringEncoding) as NSData!
+//    
+//    bodyParams.appendData(boundaryData2)
+//    
+//    // pass the caption of the image
+//    let formData = "Content-Disposition: form-data; name=\"title\"\r\n\r\n".dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
+//    bodyParams.appendData(formData!)
+//    
+//    let formData2 = title.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
+//    bodyParams.appendData(formData2!)
+//    
+//    let closingFormData = "\r\n".dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
+//    bodyParams.appendData(closingFormData!)
+//    
+//    let closingData = "--\(boundary)--\r\n"
+//    let boundaryDataEnd = closingData.dataUsingEncoding(NSUTF8StringEncoding) as NSData!
+//    
+//    bodyParams.appendData(boundaryDataEnd)
+//    
+//    request.HTTPBody = bodyParams
+//    return request
+//  }
+//  
+//  func getErrorMessage(error: NSError) -> NSString {
+//    var errorMessage : NSString
+//    
+//    // return correct error message
+//    if error.domain == "HTTPHelperError" {
+//      let userInfo = error.userInfo as NSDictionary!
+//      errorMessage = userInfo.valueForKey("message") as! NSString
+//    } else {
+//      errorMessage = error.description
+//    }
+//    
+//    return errorMessage
+//  }
     
-    let bodyParams : NSMutableData = NSMutableData()
-    
-    // build and format HTTP body with data
-    // prepare for multipart form uplaod
-    
-    let boundaryString = "--\(boundary)\r\n"
-    let boundaryData = boundaryString.dataUsingEncoding(NSUTF8StringEncoding) as NSData!
-    bodyParams.appendData(boundaryData)
-    
-    // set the parameter name
-    let imageMeteData = "Content-Disposition: attachment; name=\"image\"; filename=\"photo\"\r\n".dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
-    bodyParams.appendData(imageMeteData!)
-    
-    // set the content type
-    let fileContentType = "Content-Type: application/octet-stream\r\n\r\n".dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
-    bodyParams.appendData(fileContentType!)
-    
-    // add the actual image data
-    bodyParams.appendData(data)
-    
-    let imageDataEnding = "\r\n".dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
-    bodyParams.appendData(imageDataEnding!)
-    
-    let boundaryString2 = "--\(boundary)\r\n"
-    let boundaryData2 = boundaryString.dataUsingEncoding(NSUTF8StringEncoding) as NSData!
-    
-    bodyParams.appendData(boundaryData2)
-    
-    // pass the caption of the image
-    let formData = "Content-Disposition: form-data; name=\"title\"\r\n\r\n".dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
-    bodyParams.appendData(formData!)
-    
-    let formData2 = title.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
-    bodyParams.appendData(formData2!)
-    
-    let closingFormData = "\r\n".dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
-    bodyParams.appendData(closingFormData!)
-    
-    let closingData = "--\(boundary)--\r\n"
-    let boundaryDataEnd = closingData.dataUsingEncoding(NSUTF8StringEncoding) as NSData!
-    
-    bodyParams.appendData(boundaryDataEnd)
-    
-    request.HTTPBody = bodyParams
-    return request
-  }
-  
-  func getErrorMessage(error: NSError) -> NSString {
-    var errorMessage : NSString
-    
-    // return correct error message
-    if error.domain == "HTTPHelperError" {
-      let userInfo = error.userInfo as NSDictionary!
-      errorMessage = userInfo.valueForKey("message") as! NSString
-    } else {
-      errorMessage = error.description
+    func sharedInstance() -> HTTPHelper {
+        
+        struct Singleton {
+            static var sharedInstance = HTTPHelper()
+        }
+        return Singleton.sharedInstance
     }
-    
-    return errorMessage
-  }
 }
--- Selfie/ViewController.swift
+++ Selfie/ViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
   @IBOutlet weak var signinBackgroundView: UIView!
   @IBOutlet weak var signupBackgroundView: UIView!
   @IBOutlet weak var signinEmailTextField: UITextField!
@@ -18,15 +20,39 @@ class ViewController: UIViewController {
   @IBOutlet weak var signupPasswordTextField: UITextField!
   @IBOutlet weak var activityIndicatorView: UIView!
   @IBOutlet weak var passwordRevealBtn: UIButton!
+  @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var searchField: UITextField!
   
-  let httpHelper = HTTPHelper()
-  
+let cllocationManager: CLLocationManager = CLLocationManager()
+  var httpHelper = HTTPHelper()
+
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     
-    self.activityIndicatorView.layer.cornerRadius = 10
-  }
+//    self.activityIndicatorView.layer.cornerRadius = 10
+    mapView.delegate = self
+    
+    self.cllocationManager.requestAlwaysAuthorization()
+    self.cllocationManager.requestWhenInUseAuthorization()
+    
+    cllocationManager.startUpdatingLocation()
+    mapView.showsUserLocation = true
+    
+    let httpRequest = httpHelper.buildRequest("charities", method: "GET")
+    
+    httpHelper.sendCharitySearchRequest(httpRequest, completion: {(response:[CLLocationCoordinate2D]!, error: NSError!) in
+        
+        guard error == nil else {
+            return
+        }
+    })
+    
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
+    }
   
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
@@ -44,6 +70,67 @@ class ViewController: UIViewController {
       self.signupPasswordTextField.secureTextEntry = true
     }
   }
+    
+    
+    func populateMapData(newCoordArr:[CLLocationCoordinate2D]) {
+        
+        if !mapView.annotations.isEmpty{
+            mapView.removeAnnotations(mapView.annotations)
+        }
+        
+        let coordinateArray = newCoordArr
+        
+        var annotations = [MKPointAnnotation]()
+        print(coordinateArray)
+        
+        for s in  coordinateArray {
+            
+            self.cllocationManager.delegate = self
+            
+            /* Get the lat and lon values to create a coordinate */
+            let lat = CLLocationDegrees(s.latitude)
+            let lon = CLLocationDegrees(s.longitude)
+            let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+            
+            /* Make the map annotation with the coordinate and other student data */
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            
+            /* Add the annotation to the array */
+            annotations.append(annotation)
+            /*Append cllocations to represent overlay connections between annotations*/
+            
+            /*Load annotations/overlay to map view once data is completely loaded*/
+            dispatch_async(dispatch_get_main_queue(), {
+                self.mapView.addAnnotations(annotations)
+                self.mapView.showAnnotations(annotations, animated: true)
+            })
+        }
+    }
+
+    
+  func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseId = "pin"
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            if #available(iOS 9.0, *) {
+                pinView!.pinTintColor = UIColor.greenColor()
+            }else {
+                // Fallback on earlier versions
+            }
+            pinView!.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure)
+            pinView?.enabled = true
+            
+        }else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
   
   func displaSigninView () {
     self.signinEmailTextField.text = nil
@@ -111,53 +198,91 @@ class ViewController: UIViewController {
   @IBAction func cancelBtnTapped(sender: AnyObject) {
     self.displaSigninView()
   }
+
+    @IBAction func searchCoordBtn() {
+        searchDBForLocation(searchField.text!)
+    }
+
+  
+    
+    
+    func searchDBForLocation(search: String) {
+        let httpRequest = httpHelper.buildRequest("search", method: "POST")
+        httpRequest.HTTPBody = "{\"search\":\"\(search)\"}".dataUsingEncoding(NSUTF8StringEncoding)
+        
+        httpHelper.sendRequest(httpRequest, completion: {(data: NSData!, error: NSError!) in
+            
+            guard error == nil else {
+                print(error)
+                return
+            }
+            do {
+                
+                let responseDict = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions()) as? NSArray
+                
+                var charityLocations: [CLLocationCoordinate2D] = []
+                for coordinate in responseDict!{
+                    let json = coordinate as? [String:AnyObject]
+                    let coordinatesToAppend = CLLocationCoordinate2D(latitude: (json!["lat"]! as? Double)!, longitude: (json!["lng"]! as? Double)!)
+                    charityLocations.append(coordinatesToAppend)
+                }
+                print(charityLocations)
+                self.populateMapData(charityLocations)
+                
+            } catch let error as NSError {
+                print(error)
+            }
+            
+        })
+        
+    }
   
   
-  @IBAction func signupBtnTapped(sender: AnyObject) {
-    // Code to hide the keyboards for text fields
-    if self.signupNameTextField.isFirstResponder() {
-      self.signupNameTextField.resignFirstResponder()
-    }
-    
-    if self.signupEmailTextField.isFirstResponder() {
-      self.signupEmailTextField.resignFirstResponder()
-    }
-    
-    if self.signupPasswordTextField.isFirstResponder() {
-      self.signupPasswordTextField.resignFirstResponder()
-    }
-    
-    self.activityIndicatorView.hidden = false
-    
-    if self.signupNameTextField.text != "" && self.signupEmailTextField.text != ""
-        && self.signupPasswordTextField.text != ""{
-        makeSignUpRequest(self.signupNameTextField.text!, userEmail: self.signupEmailTextField.text!,
-                          userPassword: self.signupPasswordTextField.text!)
-    } else {
-        self.displayAlertMessage("Parameters Required", alertDescription:
-            "Some of the required parameters are missing")
-    }
-  }
+//  @IBAction func signupBtnTapped(sender: AnyObject) {
+//    // Code to hide the keyboards for text fields
+//    if self.signupNameTextField.isFirstResponder() {
+//      self.signupNameTextField.resignFirstResponder()
+//    }
+//    
+//    if self.signupEmailTextField.isFirstResponder() {
+//      self.signupEmailTextField.resignFirstResponder()
+//    }
+//    
+//    if self.signupPasswordTextField.isFirstResponder() {
+//      self.signupPasswordTextField.resignFirstResponder()
+//    }
+//    
+//    self.activityIndicatorView.hidden = false
+//    
+//    if self.signupNameTextField.text != "" && self.signupEmailTextField.text != ""
+//        && self.signupPasswordTextField.text != ""{
+//        makeSignUpRequest(self.signupNameTextField.text!, userEmail: self.signupEmailTextField.text!,
+//                          userPassword: self.signupPasswordTextField.text!)
+//    } else {
+//        self.displayAlertMessage("Parameters Required", alertDescription:
+//            "Some of the required parameters are missing")
+//    }
+//  }
   
-  @IBAction func signinBtnTapped(sender: AnyObject) {
-    // resign the keyboard for text fields
-    if self.signinEmailTextField.isFirstResponder() {
-      self.signinEmailTextField.resignFirstResponder()
-    }
-    
-    if self.signinPasswordTextField.isFirstResponder() {
-      self.signinPasswordTextField.resignFirstResponder()
-    }
-    
-    self.activityIndicatorView.hidden = false
-    
-    if self.signinEmailTextField.text != "" && self.signinPasswordTextField != "" {
-        makeSignInRequest(self.signinEmailTextField.text!, userPassword: self.signinPasswordTextField.text!)
-    }else {
-        self.displayAlertMessage("Parameters Required", alertDescription:
-            "Some of the required parameters are missing")
-    }
-  }
+//  @IBAction func signinBtnTapped(sender: AnyObject) {
+//    // resign the keyboard for text fields
+//    if self.signinEmailTextField.isFirstResponder() {
+//      self.signinEmailTextField.resignFirstResponder()
+//    }
+//    
+//    if self.signinPasswordTextField.isFirstResponder() {
+//      self.signinPasswordTextField.resignFirstResponder()
+//    }
+//    
+//    self.activityIndicatorView.hidden = false
+//    
+//    if self.signinEmailTextField.text != "" && self.signinPasswordTextField != "" {
+//        makeSignInRequest(self.signinEmailTextField.text!, userPassword: self.signinPasswordTextField.text!)
+//    }else {
+//        self.displayAlertMessage("Parameters Required", alertDescription:
+//            "Some of the required parameters are missing")
+//    }
+//  }
   
   func updateUserLoggedInFlag() {
     let defaults = NSUserDefaults.standardUserDefaults()
@@ -172,67 +297,76 @@ class ViewController: UIViewController {
         
         if key == "api_authtoken" {
 //            KeyChainAccess.setPassword(obj, account: "Auth_Token", service: "KeyChainService")
-            puts("Got api token")
+            puts(key)
         }
         
         if key == "authtoken_expiry" {
 //            KeyChainAccess.setPassword(obj, account: "Auth_Token_Expiry", service: "KeyChainService")
-            puts("Got api token expiry")
+            puts(obj)
         }
         })
     
-    self.dismissViewControllerAnimated(true, completion: nil)
+//    self.dismissViewControllerAnimated(true, completion: nil)
+    puts("Login Successful")
   }
   
-  func makeSignUpRequest(userName:String, userEmail:String, userPassword:String) {
-    let httpRequest = httpHelper.buildRequest("signup", method: "POST", authType: HTTPRequestAuthType.HTTPBasicAuth)
+//  func makeSignUpRequest(userName:String, userEmail:String, userPassword:String) {
+//    let httpRequest = httpHelper.buildRequest("signup", method: "POST", authType: HTTPRequestAuthType.HTTPBasicAuth)
+//    
+//    let encrypted_password = AESCrypt.encrypt(userPassword, password: HTTPHelper.API_AUTH_PASSWORD)
+//    
+//    httpRequest.HTTPBody = "{\"full_name\":\"\(userName)\",\"email\":\"\(userEmail)\",\"password\":\"\(encrypted_password)\"}".dataUsingEncoding(NSUTF8StringEncoding)
+//    
+//    httpHelper.sendRequest(httpRequest, completion: {(data: NSData!, error: NSError!) in
+//        
+//        guard error == nil else {
+//            let errorMessage = self.httpHelper.getErrorMessage(error)
+//            self.displayAlertMessage("Error", alertDescription: errorMessage as String)
+//            return
+//        }
+//        
+//        self.displaSigninView()
+//        self.displayAlertMessage("Success", alertDescription: "Acount successfully created!")
+//        
+//        })
+//  }
+//  
+//    func makeSignInRequest(userEmail:String, userPassword:String) {
+//        let httpRequest = httpHelper.buildRequest("signin", method: "POST", authType: HTTPRequestAuthType.HTTPBasicAuth)
+//        
+//        let encrypted_password = AESCrypt.encrypt(userPassword, password: HTTPHelper.API_AUTH_PASSWORD)
+//        
+//        httpRequest.HTTPBody = "{\"email\":\"\(userEmail)\",\"password\":\"\(encrypted_password)\"}".dataUsingEncoding(NSUTF8StringEncoding)
+//        
+//        httpHelper.sendRequest(httpRequest, completion: {(data: NSData!, error: NSError!) -> Void in
+//            
+//            guard error == nil else {
+//                let errorMessage = self.httpHelper.getErrorMessage(error)
+//                self.displayAlertMessage("Error", alertDescription: errorMessage as String)
+//                return
+//            }
+//            
+//            do {
+//            
+//            self.activityIndicatorView.hidden = true
+//            self.updateUserLoggedInFlag()
+//        
+//            let responseDict = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments) as! NSDictionary
+//                
+//            self.saveApiTokenInKeychain(responseDict)
+//            }
+//            
+//            catch let completionError as NSError {
+//                print(completionError)
+//            }
+//        })
+//}
     
-    let encrypted_password = AESCrypt.encrypt(userPassword, password: HTTPHelper.API_AUTH_PASSWORD)
-    
-    httpRequest.HTTPBody = "{\"full_name\":\"\(userName)\",\"email\":\"\(userEmail)\",\"password\":\"\(encrypted_password)\"}".dataUsingEncoding(NSUTF8StringEncoding)
-    
-    httpHelper.sendRequest(httpRequest, completion: {(data: NSData!, error: NSError!) in
+    static func sharedInstance() -> ViewController {
         
-        guard error == nil else {
-            let errorMessage = self.httpHelper.getErrorMessage(error)
-            self.displayAlertMessage("Error", alertDescription: errorMessage as String)
-            return
+        struct Singleton {
+            static var sharedInstance = ViewController()
         }
-        
-        self.displaSigninView()
-        self.displayAlertMessage("Success", alertDescription: "Acount successfully created!")
-        
-        })
-  }
-  
-    func makeSignInRequest(userEmail:String, userPassword:String) {
-        let httpRequest = httpHelper.buildRequest("signin", method: "POST", authType: HTTPRequestAuthType.HTTPBasicAuth)
-        
-        let encrypted_password = AESCrypt.encrypt(userPassword, password: HTTPHelper.API_AUTH_PASSWORD)
-        
-        httpRequest.HTTPBody = "{\"email\":\"\(userEmail)\",\"password\":\"\(encrypted_password)\"}".dataUsingEncoding(NSUTF8StringEncoding)
-        
-        httpHelper.sendRequest(httpRequest, completion: {(data: NSData!, error: NSError!) -> Void in
-            
-            guard error == nil else {
-                let errorMessage = self.httpHelper.getErrorMessage(error)
-                self.displayAlertMessage("Error", alertDescription: errorMessage as String)
-                return
-            }
-            
-            do {
-            
-            self.activityIndicatorView.hidden = true
-            self.updateUserLoggedInFlag()
-        
-            let responseDict = try NSJSONSerialization.JSONObjectWithData(data,
-                                                                      options: NSJSONReadingOptions.AllowFragments) as! NSDictionary
-            self.saveApiTokenInKeychain(responseDict)
-            }
-            
-            catch let completionError as NSError {
-                print(completionError)
-            }
-        })
-}
+        return Singleton.sharedInstance
+    }
 }
