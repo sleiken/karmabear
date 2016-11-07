d57d01b0627319f48f91dc0eada22b4101494009
Ali Baker <email": null>
2016-11-07 11:40:38 -0800

    Change manager routes - donations, registrations, givers

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,41 @@
 Rails.application.routes.draw do
-  root 'main#index'
-  get 'home', to: 'main#index'
+  # root 'main#index'
+  # get 'home', to: 'main#index'
 
-  get 'users/:username', to: 'givers#show', as: :giver
+  # get 'users/:username', to: 'givers#show', as: :giver
 
-  devise_for :managers
-  devise_for :givers, :controllers => { :omniauth_callbacks => "givers_omniauth_callbacks" }
+  # devise_for :managers
+  # devise_for :givers, :controllers => { :omniauth_callbacks => "givers_omniauth_callbacks" }
 
   namespace :manager do
     root 'charities#show'
 
-    resources :charities, only: [:show, :edit, :update] do
+    resources :charities, only: [:show] do
       resources :givers, only: [:index, :show]
       resources :events do
-        resources :registrations
+        resources :givers, only: [:index, :show]
+        resources :registrations, only: [:index, :show, :edit, :update]
       end
       resources :needs do
-        resources :donations
+        resources :givers, only: [:index, :show]
+        resources :donations, only: [:index, :show, :edit, :update]
       end
     end
   end
 
-  resources :charities, only: [:index, :show] do
-    resources :subscriptions, only: [:create, :destroy]
-    resources :events, only: [:index, :show] do
-      resources :registrations, except: :index
-    end
-    resources :needs, only: [:index, :show] do
-      resources :donations, except: :index
-    end
-  end
+  # resources :charities, only: [:index, :show] do
+  #   resources :subscriptions, only: [:create, :destroy]
+  #   resources :events, only: [:index, :show] do
+  #     resources :registrations, except: :index
+  #   end
+  #   resources :needs, only: [:index, :show] do
+  #     resources :donations, except: :index
+  #   end
+  # end
 
-  namespace :api, defaults: { format: :json } do
-    post 'search', to: 'api#search'
-    get 'charities', to: 'api#charities'
-    get 'giver_profile', to: 'api#giver_profile'
-  end
+  # namespace :api, defaults: { format: :json } do
+  #   post 'search', to: 'api#search'
+  #   get 'charities', to: 'api#charities'
+  #   get 'giver_profile', to: 'api#giver_profile'
+  # end
 end
