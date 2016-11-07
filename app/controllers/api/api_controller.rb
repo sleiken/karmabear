90e7a10a8a954460a78ca31aae8a6dd9f9a08b24
Sam Leiken <sam.k.leiken@gmail.com>
2016-11-07 13:06:18 -0800

    Remove before_action on api controller

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,6 +1,5 @@
 class Api::ApiController < ApplicationController
   skip_before_action :verify_authenticity_token
-  before_action :authorize_user, only: [:giver_profile]
   respond_to :json
 
   def search
