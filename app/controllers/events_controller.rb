6ecb4adaf872d643feb0576fa69c388ed85d489e
Ali Baker <email": null>
2016-11-07 16:29:06 -0800

    Remove commented out index action as there will never be need for /events route, only need of rendering an events partial

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,4 @@
 class EventsController < ApplicationController
-  # def index
-  #   @charity = Charity.find(params[:charity_id])
-  #   @events = @charity.events
-  # end
-
   def show
     @charity = Charity.find(params[:charity_id])
     @event = Event.find(params[:id])
