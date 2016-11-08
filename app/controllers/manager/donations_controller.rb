d4a22302190a5ac454eaabf1e2a5a8a42aaa5b75
Ali Baker <email": null>
2016-11-08 14:48:51 -0800

    Add approve and deny buttons that route to create and destroy in the donations and registrations show views and controllers

--- app/controllers/manager/donations_controller.rb
+++ app/controllers/manager/donations_controller.rb
@@ -4,18 +4,23 @@ class Manager::DonationsController < Manager::ApplicationController
   end
 
   def show
-
+    @donation = Donation.find(params[:id])
+    @charity = Charity.find(params[:charity_id])
   end
 
   def update
+    @donation = Donation.find(params[:id])
+    @charity = Charity.find(params[:charity_id])
     # after manager clicks "approve":
     # update user points on user object
     # close modal with js
+    redirect_to '/'
   end
 
   def destroy
      # after manager clicks "deny":
      # destroy donation object
      # close modal with js
+    redirect_to '/'
   end
 end
--- app/controllers/manager/registrations_controller.rb
+++ app/controllers/manager/registrations_controller.rb
@@ -4,19 +4,24 @@ class Manager::RegistrationsController < Manager::ApplicationController
   end
 
   def show
-
+    @registration = Registration.find(params[:id])
+    @charity = Charity.find(params[:charity_id])
   end
 
   def update
+    @registration = Registration.find(params[:id])
+    @charity = Charity.find(params[:charity_id])
     # after manager clicks "approve":
     # add pending_hours to approved_hours
     # update user points
     # close modal with js
+    redirect_to '/'
   end
 
   def destroy
      # after manager clicks "deny":
      # destroy registration object
      # close modal with js
+     redirect_to '/'
   end
 end
