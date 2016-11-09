393070f0dd76485d464cfb6594c930d2f31164c9
Sam Leiken <sam.k.leiken@gmail.com>
2016-11-08 18:06:44 -0800

    Add migration and model for notifications and action cable channel for dash

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161108202848) do
+ActiveRecord::Schema.define(version: 20161109020304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,6 +119,14 @@ ActiveRecord::Schema.define(version: 20161108202848) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.integer  "status",          default: 1
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "giver_id"
+    t.integer  "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "registrations", force: :cascade do |t|
