c1fb0d08aa7f3e33b00d6409d1006933a0608535
Sam Leiken <sam.k.leiken@gmail.com>
2016-11-09 10:10:17 -0800

    Rough implementation of manager-specific channels

--- app/channels/dash_channel.rb
+++ app/channels/dash_channel.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class DashChannel < ApplicationCable::Channel
   def subscribed
-    stream_from 'dash_channel'
+    stream_from "dash_channel_#{$current_manager[:id]}"
   end
 
   def unsubscribed
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,13 +3,12 @@ class Notification < ApplicationRecord
   belongs_to :manager
   validates :content, presence: true
   scope :for_display, -> { order(:created_at).last(50) }
-  # after_create_commit { EventBroadcastJob.perform_later self }
   after_create_commit { broadcast_notification(self) }
 
   private
 
   def broadcast_notification(notification)
-    ActionCable.server.broadcast "dash_channel", message: render_notification(notification)
+    ActionCable.server.broadcast "dash_channel_#{notification.manager_id}", message: render_notification(notification)
   end
 
   def render_notification(notification)
