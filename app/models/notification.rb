0a834eed27d17c56924ff84c3f6208d9b0a7573e
Sam Leiken <sam.k.leiken@gmail.com>
2016-11-09 10:03:40 -0800

    Move broadcast logic to notification model

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,5 +3,16 @@ class Notification < ApplicationRecord
   belongs_to :manager
   validates :content, presence: true
   scope :for_display, -> { order(:created_at).last(50) }
-  after_create_commit { EventBroadcastJob.perform_later self }
+  # after_create_commit { EventBroadcastJob.perform_later self }
+  after_create_commit { broadcast_notification(self) }
+
+  private
+
+  def broadcast_notification(notification)
+    ActionCable.server.broadcast "dash_channel", message: render_notification(notification)
+  end
+
+  def render_notification(notification)
+    ApplicationController.renderer.render(partial: 'notifications/event', locals: { notification: notification })
+  end
 end
