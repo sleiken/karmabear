acc75e0655909238bd4c8e6021e9633bcfc6a837
Sam Leiken <sam.k.leiken@gmail.com>
2016-11-10 13:57:55 -0800

    Add scss file for modals

--- app/mailers/manager_mailer.rb
+++ app/mailers/manager_mailer.rb
@@ -8,7 +8,8 @@ class ManagerMailer < ApplicationMailer
 
   def follower_email(manager, subject, body)
     recipients = manager.charity.followers
-    mail(bcc: recipients.map(&:email).uniq,
+    recipients.map(&:email).uniq
+    mail(bcc: "sam.k.leiken@gmail.com",
          subject: subject,
          body: body)
   end
