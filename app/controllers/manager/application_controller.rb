ce62132cbe0e5658c35e2eef7a8bc04fb25c43a6
Sam Leiken <sam.k.leiken@gmail.com>
2016-11-10 19:08:24 -0800

    Secures manager authorization

--- app/controllers/manager/application_controller.rb
+++ app/controllers/manager/application_controller.rb
@@ -1,12 +1,16 @@
 class Manager::ApplicationController < ApplicationController
-  # before_action :authorize_manager, except: [:]
-  #
-  # private
-  #
-  # def authorize_manager
-  #   manager_id = Charity.find(params)
-  #   if current_manager != Manager.find_by!(username: params[:username])
-  #     redirect_to root_path
-  #   end
-  # end
+  before_action :authorize_manager
+
+  protected
+
+  def authorize_manager
+    if Charity.find_by!(id: params[:charity_id]).manager
+      manager_id = Charity.find_by!(id: params[:charity_id]).manager.id
+      if current_manager != Manager.find_by!(id: manager_id)
+        redirect_to root_path
+      end
+    else
+      redirect_to root_path
+    end
+  end
 end
--- app/controllers/manager/charities_controller.rb
+++ app/controllers/manager/charities_controller.rb
@@ -1,5 +1,6 @@
 class Manager::CharitiesController < Manager::ApplicationController
-	before_action :authorize_manager
+	skip_before_action :authorize_manager
+	before_action :authorize_manager_show
 
 	def show
 		@manager = current_manager
@@ -22,13 +23,13 @@ class Manager::CharitiesController < Manager::ApplicationController
 
 	private
 
-  def authorize_manager
+  def authorize_manager_show
 		if Charity.find_by!(id: params[:id]).manager
     	manager_id = Charity.find_by!(id: params[:id]).manager.id
 	    if current_manager != Manager.find_by!(id: manager_id)
 	      redirect_to root_path
 	    end
-		else 
+		else
 			redirect_to root_path
 		end
   end
