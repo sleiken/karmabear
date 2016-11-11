b6a5410202bc7549c7d1c44ec8e5637ef0e2ceb9
Sam Leiken <sam.k.leiken@gmail.com>
2016-11-10 23:46:13 -0800

    Add facebook login button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(manager)
-    manager_charity_path(current_manager.charity.id)
+    if current_manager
+      manager_charity_path(current_manager.charity.id)
+    else
+      root_path
+    end
   end
 end
